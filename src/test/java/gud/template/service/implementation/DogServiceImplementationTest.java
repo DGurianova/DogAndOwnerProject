c2bcb9e7fb4c976520ae4c69fc39e23458875855
DGurianova <daria.gurianova1@gmail.com>
2022-07-06 20:31:10 +0200

    Tests added

--- src/test/java/gud/template/service/implementation/DogServiceImplementationTest.java
+++ src/test/java/gud/template/service/implementation/DogServiceImplementationTest.java
@@ -1,15 +1,25 @@
 package gud.template.service.implementation;
 
 import gud.template.dto.DogRequestDTO;
+import gud.template.dto.DogResponseDTO;
 import gud.template.entity.Dog;
+import gud.template.entity.Owner;
 import gud.template.repository.DogRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
 import org.mockito.Mockito;
-import org.mockito.internal.matchers.Any;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.when;
+
 
 class DogServiceImplementationTest {
     @MockBean
@@ -46,7 +56,7 @@ class DogServiceImplementationTest {
         service.setDogRepository(mockDogRepository);
 
 
-        Mockito.when(mockDogRepository.save(Mockito.any(Dog.class))).then(i -> i.getArgument(0));
+        when(mockDogRepository.save(Mockito.any(Dog.class))).then(i -> i.getArgument(0));
         //WHEN
         Dog dogCreatedByService = service.createDog(dto);
 
@@ -57,7 +67,128 @@ class DogServiceImplementationTest {
 
     }
 
+    @Test
+    void getDogById() {
+        DogResponseDTO dto = new DogResponseDTO(
+                1L,
+                "Hatiko",
+                "Laika",
+                "01.01.2021",
+                null,
+                null
+        );
+        Dog dogManyallyCreated = Dog.builder()
+                .nickname("Hatiko")
+                .breed("Laika")
+                .dateOfBirth("01.01.2021")
+                .registrationDate(null)
+                .build();
+        service.setDogRepository(mockDogRepository);
 
-//    @Test
-//    void getAllUnregisteredDogs() {
+        when(mockDogRepository.findById(anyLong())).thenReturn(Optional.of(dogManyallyCreated));
+
+        DogResponseDTO dogCreatedByService = service.getDogById(1L);
+
+        assertEquals(dogCreatedByService.getNickname(), dogManyallyCreated.getNickname());
+        assertEquals(dogCreatedByService.getBreed(), dogManyallyCreated.getBreed());
+        assertEquals(dogCreatedByService.getDateOfBirth(), dogManyallyCreated.getDateOfBirth());
+
+
+    }
+
+    @Test
+    void getDogByIdWhenThereIsNoSuchDogExists() {
+        DogResponseDTO dto = new DogResponseDTO(
+                null,
+                "NotExists",
+                "NoSuchDog",
+                null,
+                null,
+                null
+        );
+        service.setDogRepository(mockDogRepository);
+
+        when(mockDogRepository.findById(anyLong())).thenReturn(Optional.empty());
+        ResponseStatusException aThrows = assertThrows(ResponseStatusException.class, () -> {
+            service.getDogById(1l);
+        });
+
+
+
+    }
+
+    @Test
+    void getAllDogs() {
+        Dog dogOne = Dog.builder()
+                .nickname("Hatiko")
+                .breed("Laika")
+                .dateOfBirth("01.01.2021")
+                .registrationDate(null)
+                .build();
+
+        Dog dogTwo = Dog.builder()
+                .nickname("Leo")
+                .breed("Jack-Russel")
+                .dateOfBirth("01.04.2021")
+                .registrationDate("23.09.2021")
+                .build();
+        service.setDogRepository(mockDogRepository);
+
+        List<Dog> dogsList = new ArrayList<>();
+        dogsList.add(dogOne);
+        dogsList.add(dogTwo);
+
+        when(mockDogRepository.findAll()).thenReturn(dogsList);
+
+        List<DogResponseDTO> result = service.getAllDogs();
+
+        DogResponseDTO dogOneResponseDTO = result.get(0);
+        assertEquals(dogOneResponseDTO.getNickname(), dogOne.getNickname());
+        assertEquals(dogOneResponseDTO.getBreed(), dogOne.getBreed());
+        assertEquals(dogOneResponseDTO.getDateOfBirth(), dogOne.getDateOfBirth());
+
+        DogResponseDTO dogTwoResponseDTO = result.get(1);
+        assertEquals(dogTwoResponseDTO.getNickname(), dogTwo.getNickname());
+        assertEquals(dogTwoResponseDTO.getBreed(), dogTwo.getBreed());
+        assertEquals(dogTwoResponseDTO.getDateOfBirth(), dogTwo.getDateOfBirth());
+
+    }
+
+    @Test
+    void getAllUnregisteredDogs() {
+        Dog dogOne = Dog.builder()
+                .nickname("Hatiko")
+                .breed("Laika")
+                .dateOfBirth("01.01.2021")
+                .registrationDate(null)
+                .owner(null)
+                .build();
+
+        Owner ownerOfDogTwo = new Owner(1L, "Nikita", "Nikitin", "12.10.1990");
+
+        Dog dogTwo = Dog.builder()
+                .nickname("Leo")
+                .breed("Jack-Russel")
+                .dateOfBirth("01.04.2021")
+                .registrationDate("23.09.2021")
+                .owner(ownerOfDogTwo)
+                .build();
+        service.setDogRepository(mockDogRepository);
+
+        List<Dog> dogsList = new ArrayList<>();
+        dogsList.add(dogOne);
+        dogsList.add(dogTwo);
+
+        when(mockDogRepository.findAll()).thenReturn(dogsList);
+
+        List<DogResponseDTO> result = service.getAllUnregisteredDogs();
+
+        assertEquals(1, result.size());
+
+        DogResponseDTO dogOneResponseDTO = result.get(0);
+        assertEquals(dogOne.getNickname(), dogOneResponseDTO.getNickname());
+        assertEquals(dogOne.getBreed(), dogOneResponseDTO.getBreed());
+        assertEquals(dogOne.getDateOfBirth(), dogOneResponseDTO.getDateOfBirth());
+        assertEquals(dogOne.getOwner(), dogOneResponseDTO.getOwner());
+    }
 }
